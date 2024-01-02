1669443ed887704e16440d795948776417b2b225
davis <davis@davismcd.com>
2024-01-01 20:14:58 -0700

    stuff

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 from math import ceil
 from glob import glob
-
+from re import search, split, findall
 
 class Parser:
     def __init__(self, codetext, indent='    '):
@@ -17,8 +17,59 @@ class Parser:
                     break
                 indention += 1
             line = line[indention * len(self.indent):]
+            if search("^[a-zA-Z][a-zA-Z0-9]* *=", line):
+                self.parsed.append({
+                    'type': 'set var',
+                    'name':  findall("^[a-zA-Z][a-zA-Z0-9]* *=", line)[0][:-2].strip(),
+                    'value': Parser.parse_expression(split("^[a-zA-Z][a-zA-Z0-9]* *=", line, 1)[1].strip())
+                })
         return self.parsed
 
+    @classmethod
+    def parse_expression(cls, expression):
+        current = ""
+        num_par = 0
+        new_expression = ""
+        replaces = []
+        for char in expression:
+            if char == '(':
+                num_par += 1
+                continue
+            if char == ')':
+                num_par -= 1
+                if num_par == 0:
+                    new_expression += f'|:{len(replaces)}:|'
+                    replaces.append(cls.parse_expression(current))
+                    current = ""
+                continue
+            if 0 < num_par:
+                current += char
+                continue
+            new_expression += char
+        expression = new_expression
+        new_expression = ""
+        operator = None
+        num1 = ""
+        current = ""
+        for char in expression:
+            if char in ['*', '/', '+', '-']:
+                if operator:
+                    new_expression += f'|:{len(replaces)}:|'
+                    replaces.append({
+                        'type': operator,
+                        'num1': cls.parse_expression(num1),
+                        'num2': cls.parse_expression(current)
+                    })
+                num1 = current
+                current = ''
+            if
 
-for file in glob('*.🐮🦬'):
+        return None
+
+files = glob('*.🐮🦬')
+files.extend(glob('*.🦬🐮'))
+files.extend(glob('*.🔥🦬'))
+files.extend(glob('*.cb'))
+files.extend(glob('*.fb'))
+for file in files:
     print(Parser(open(file).read()).parse())
