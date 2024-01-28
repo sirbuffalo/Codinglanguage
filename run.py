59f17fd485ab7f32a333180cb1041758cc5d3999
Ian Gulliver <github@flamingcow.io>
2024-01-28 10:04:54 -0800

    Pretty-print json

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
+import json
 from parser import Parser
 from interpreter import Interpreter
 
@@ -12,6 +13,6 @@ args = ap.parse_args()
 for filename in args.filename:
     parsed = Parser(filename).parse()
     if args.parsed:
-        print(f'{filename}: {parsed}')
+        print(f'{filename}: {json.dumps(parsed, indent=4)}')
     interp = Interpreter(parsed)
     interp.run()
